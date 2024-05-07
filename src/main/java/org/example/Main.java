f081d8d75e6c5a4c9a0047bc18b6bafb63304bee
lopez <c.lopezjoshua@gmail.com>
2024-05-06 21:58:46 -0600

    Used dependency injection to optimize spring usage

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-
 import java.io.IOException;
 import java.time.LocalTime;
 
